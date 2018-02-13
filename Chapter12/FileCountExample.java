cce57902ab4784b6acda20b8ff16aae60df31b4c
sb24029 <Subranil25@gmail.com>
2018-02-13 14:26:54 -0500

    Exercise13

---
+++ Chapter12/FileCountExample.java
@@ -0,0 +1,55 @@
+package Chapter12;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.Scanner;
+
+public class FileCountExample {
+
+   public static void main(String[] args) {
+       System.out.println("Please enter the filename: ");
+   Scanner input = new Scanner(System.in);
+     
+   String fileName = input.nextLine();
+     
+   FileReader fReader;
+   try {
+   fReader = new FileReader(fileName);
+   BufferedReader reader = new BufferedReader(fReader);
+   String cursor; //
+   String content = "";
+   int lines = 0;
+   int words = 0;
+   int chars = 0;
+   while((cursor = reader.readLine()) != null){
+   // count lines
+   lines += 1;
+   content += cursor;
+     
+   // count words
+   String []_words = cursor.split(" ");
+   for( String w : _words)
+   {
+   words++;
+   }
+     
+   }
+   chars = content.length();
+     
+   System.out.println("File " + fileName + " has ");
+   System.out.println(chars + " Characters,");
+   System.out.println(words + " words and " + lines + " lines.");
+     
+     
+   } catch (FileNotFoundException ex) {
+   // Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+   System.out.println("File not found!");
+   } catch (IOException ex) {
+   //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+   System.out.println("An error has occured: " + ex.getMessage());
+   }
+   }
+}
